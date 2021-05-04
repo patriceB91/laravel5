bdbfbcd128cb9ad17d9ab4cd228585ee01e116bf
patriceB91 <34796847+patriceB91@users.noreply.github.com>
2021-05-04 17:40:01 +0200

    Added about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </head>
+    <body class="antialiased">
+
+        <p> Build by PB </p>
+
+
+        <a href="/" class="ml-1 underline">Retour acceuil </a>
+
+        </div>
+    </body>
+</html>
