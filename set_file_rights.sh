81271f965feb0df765421d667baf282eec8b5c2f
patriceB91 <34796847+patriceB91@users.noreply.github.com>
2021-05-17 19:11:57 +0200

    Updated set file rights

--- set_file_rights.sh
+++ set_file_rights.sh
@@ -2,17 +2,17 @@
 # To be run on the synology. Adjust vars below.
 # Purpose is to reset all laravle file to what it should
 # Copy this file one level above and allow +x
-LARAVEL_PATH="/Volume2/web/kiosksadmin"
+LARAVEL_PATH="/volume2/web/kiosksadmin"
 USER="patrice"
 GROUP="http"
 
-sudo chown -R {$USER}:{$GROUP} {$LARAVEL_PATH}
+sudo chown -R ${USER}:${GROUP} ${LARAVEL_PATH}
 
-sudo find {$LARAVEL_PATH} -type f -exec chmod 644 {} \;
+sudo find ${LARAVEL_PATH} -type f -exec chmod 644 {} \;
 
-sudo find {$LARAVEL_PATH} -type d -exec chmod 755 {} \;
+sudo find ${LARAVEL_PATH} -type d -exec chmod 755 {} \;
 
-sudo chgrp -R {$USER} storage bootstrap/cache
-sudo chmod -R ug+rwx storage bootstrap/cache
+sudo chgrp -R ${GROUP} ${LARAVEL_PATH}/storage ${LARAVEL_PATH}/bootstrap/cache
+sudo chmod -R ug+rwx ${LARAVEL_PATH}/storage ${LARAVEL_PATH}/bootstrap/cache
 # Allow file upload
-sudo chmod 775 {$LARAVEL_PATH}/public/kiosks
+sudo chmod 775 ${LARAVEL_PATH}/public/kiosks
